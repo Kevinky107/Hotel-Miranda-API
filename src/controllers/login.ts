5fa2e1049f428cdfee420a1b4e5ce0f50f287972
Kevin Agudo Montil <121159576+Kevinky107@users.noreply.github.com>
2024-08-20 17:35:13 +0200

    Update login.ts

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -6,12 +6,22 @@ import bcrypt from 'bcryptjs';
 
 export const LoginController = express.Router();
 
+interface userdata {
+    email : string | null,
+    password : string | null, 
+    name: string | null,
+    picture : string | null
+}
+
+let userChecked: userdata = {email: null, password: null, name: null, picture: null};
+
 LoginController.post('/', async(req: Request, res: Response, next: NextFunction) => {
     const { email, password } = req.body;
     const check = await checkUser(email, password)
     if (check) {
         const token = jwt.sign({ email, password }, process.env.TOKEN_SECRET || 'secretKey', { expiresIn: '1h' })
-        res.json({Token: token})
+        userChecked.password = password;
+        res.json({Token: token, User: userChecked})
     } else {
         const error = new APIError('Invalid credentials', 401);
         next(error);
@@ -21,8 +31,10 @@ LoginController.post('/', async(req: Request, res: Response, next: NextFunction)
 async function checkUser(email: string, password: string): Promise<boolean> {
     try {
         const user = await UserModel.findOne({email: email}).exec();
-        if(user)
+        if(user) {
+            userChecked = {email: user.email, password: user.password, name: user.name, picture: user.picture};
             return await bcrypt.compare(password, user.password);
+        }
         else
             return false  
     } catch (error) {
